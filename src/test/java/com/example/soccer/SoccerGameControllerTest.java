12809a3be5627b2aca44ed8307dc10db8308781a
nassar <assarn@objectcomputing.com>
2023-05-11 16:42:36 -0500

    imports

--- src/test/java/com/example/soccer/SoccerGameControllerTest.java
+++ src/test/java/com/example/soccer/SoccerGameControllerTest.java
@@ -17,12 +17,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static io.micronaut.http.HttpHeaders.LOCATION;
-import static io.micronaut.http.HttpStatus.BAD_REQUEST;
 import static io.micronaut.http.HttpStatus.CREATED;
 import static io.micronaut.http.HttpStatus.NOT_FOUND;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 class SoccerGameControllerTest {
